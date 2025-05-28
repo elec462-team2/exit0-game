00c2a65c5d47ececa8a865eb699fd593b3574505
JeonKangMU <babjaaa@knu.ac.kr>
2025-05-28 11:20:53 +0000

    feat: 추가된 함수 선언

--- include/server_api.h
+++ include/server_api.h
@@ -1,11 +1,13 @@
 #ifndef __SERVER_API_H__
 #define __SERVER_API_H__
 
-#include "protocol.h"
-
-// 로그인 관련
+// 로그인
+int handle_login(int client_sock, char *user_id_buf);
 int check_user_credentials(const char *userid, const char *password);
-void handle_login(int client_sock);
+
+// 자산 조회·업데이트
+int  get_user_asset(const char *userid);
+void update_user_asset(const char *userid, int new_balance);
 
 // 게임
 void handle_labor_game(int client_sock, const char *userid);
@@ -14,4 +16,4 @@ void handle_casino_game(int client_sock, const char *userid);
 // 채팅
 void handle_chat(int client_sock, const char *userid);
 
-#endif
+#endif // __SERVER_API_H__
